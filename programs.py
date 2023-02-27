a22fea4269671bbc175bec0fa7fb5a22c77b3af0
meia-noite <lordzarkares@Outlook.com>
2023-02-27 17:35:40 -0300

    |

---
+++ programs.py
@@ -0,0 +1,9 @@
+fifo = { # Programs that need a file as stdin.
+    # Example:
+    # "program_name": ["path/to/program"]
+}
+
+stream = { # Prograsm that accept text as stdin (Usually with a flag for that).
+    # Example:
+    # "program_name": ["path/to/program", "--flag_that_makes_it_accept_text_as_stdin"]
+}
